bfa8b2b39aa9a26773bade54c1094ab60830c870
jgpark-1 <jgpark7281@gmail.com>
2024-04-19 18:04:26 +0900

    feat: add example code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,26 +2,29 @@ import { HeroSection } from "@/components/widget/section/hero/hero-section"
 
 export default function Home() {
   return (
-    <HeroSection
-      contents={[
-        {
-          key: "tab1",
-          children: "tab1",
-          imageUrl: "animals1.jpg",
-        }, {
-          key: "tab2",
-          children: "tab2",
-          imageUrl: "animals2.jpg",
-        }, {
-          key: "tab3",
-          children: "tab3",
-          imageUrl: "animals3.jpg",
-        }, {
-          key: "tab4",
-          children: "tab4",
-          imageUrl: "animals4.jpg",
-        },
-      ]}
-    />
+    <>
+      <HeroSection
+        contents={[
+          {
+            key: "tab1",
+            children: "tab1",
+            imageUrl: "animals1.jpg",
+          }, {
+            key: "tab2",
+            children: "tab2",
+            imageUrl: "animals2.jpg",
+          }, {
+            key: "tab3",
+            children: "tab3",
+            imageUrl: "animals3.jpg",
+          }, {
+            key: "tab4",
+            children: "tab4",
+            imageUrl: "animals4.jpg",
+          },
+        ]}
+      />
+      <div style={{ height: "1000px" }} />
+    </>
   )
 }
